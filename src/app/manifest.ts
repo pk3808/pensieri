2422a97cd0d658c6c7113951339f08d4bc6a94a9
Piyush <piyush@ibarts.in>
2025-12-13 17:59:24 +0530

    c

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Pensieri.',
+        name: 'Pensieri',
         short_name: 'Pensieri',
         description: 'A sanctuary for thoughtful writing and deep reading.',
         start_url: '/',
